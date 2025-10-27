a4f9b4cf924fca7b92f445c2c7072af6f6f5a6f1
paulsunny90 <paulsunny87267@gmail.com>
2025-10-27 09:58:05 +0530

    completed

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
- let arr=[]
- document.getElementById("addButton").addEventListener("click",(e)=>{
-    let data =parseInt(document.getElementById("input").value)
 
-    arr.push(data)
 
- })
- console.log(arr)
+ let Todo=[];
+
+document.getElementById("add").addEventListener('click',()=>{
+    let task = document.getElementById("task").value;
+    if(task=="")
+        return alert("Enter your task name")
+    Todo.push(task)
+    document.getElementById("task").value="";
+    // display()
+})
+
+// function display(){
+//     let str =``;
+//     for(const[index,task] of Todo.entries()){
+//         str+= `
+//         <tr>
+//             <td>${index+1}</td>
+//             <td>${task}</td>
+//             <td><button id="edit">Edit</button></td>
+//             <td><button id="delete" onclick="deleteTask(${index})">Delete</button></td>
+//         </tr>
+//         ` 
+//     }
+//     document.getElementById("display").innerHTML=str
+// }
+// function deleteTask(index){
+//     let task = Todo[index];
+//     if(confirm(Are you sure "${task}" want to delete?)){
+//         Todo.splice(index,1);
+//         display()
+//     }
+// }
